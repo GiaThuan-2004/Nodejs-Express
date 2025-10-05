3ff5a55c3f2bfbbaf28210880d1698ee01f8dfcb
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-10-05 11:18:23 +0700

    use enviroment variable with dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express"
+import 'dotenv/config'
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
     res.send('Hello world With NodeMon')
@@ -9,4 +10,5 @@ app.get("/", (req, res) => {
 
 app.listen(8080, () => {
     console.log(`App is running on ${PORT}`)
+    console.log(`File .env PORT = ${process.env.PORT}`)
 })
