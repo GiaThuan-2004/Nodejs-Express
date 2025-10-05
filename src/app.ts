418488d9d97bc2b8d28a7fcb6c6d9a8771a4f893
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-10-05 11:47:45 +0700

    template Engine (with ejs)

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@ import 'dotenv/config'
 const app = express()
 const PORT = process.env.PORT || 8080
 
+// view config
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+
 app.get("/", (req, res) => {
-    res.send('Hello world With NodeMon')
+    res.render("home") // render file home.ejs
 })
 
 app.listen(8080, () => {
     console.log(`App is running on ${PORT}`)
-    console.log(`File .env PORT = ${process.env.PORT}`)
 })
