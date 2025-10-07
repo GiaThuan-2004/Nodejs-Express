3686f4281433d816a24215958592b74e9afacddf
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-10-07 14:35:23 +0700

    use controller

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,17 +1,18 @@
 import express, { Express } from "express"
+import { getCreateUserPage, getHomePage } from "../controllers/user-controller"
 
 const router = express.Router()
 
 const webRouter = (app: Express) => {
-    router.get("/", (req, res) => {
-        res.render("home") // render file home.ejs
-    })
+    router.get("/", getHomePage)
+
+    router.get("/user", getCreateUserPage)
 
     router.get("/myLink", (req, res) => {
         res.send("This is new Link")
     })
 
-    app.use("/router", router) //giong @RequestMapping (nhom cac url co phuc vu )
+    app.use("/", router) //giong @RequestMapping (nhom cac url co phuc vu )
 }
 
 export default webRouter
